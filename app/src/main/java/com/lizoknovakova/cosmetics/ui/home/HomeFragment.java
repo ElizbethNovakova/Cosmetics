d9f2142d856979ca90886e936bd9120828188648
ElizbethNovakova <lizochka1211@gmail.com>
2020-07-28 17:03:39 +0300

    Adapter for list

--- app/src/main/java/com/lizoknovakova/cosmetics/ui/home/HomeFragment.java
+++ app/src/main/java/com/lizoknovakova/cosmetics/ui/home/HomeFragment.java
@@ -1,24 +1,23 @@
 package com.lizoknovakova.cosmetics.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.lizoknovakova.cosmetics.ContentActivity;
+import com.lizoknovakova.cosmetics.itemadapter.CosmeticItem;
+import com.lizoknovakova.cosmetics.itemadapter.DataAdapter;
 import com.lizoknovakova.cosmetics.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -30,6 +29,8 @@ public class HomeFragment extends Fragment {
     private final String CATEGORY_KEY = "CATEGORY_KEY";
     private final String POSITION_KEY = "POSITION_KEY";
 
+    private List<CosmeticItem> cosmeticItems = new ArrayList<>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,20 +38,31 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        listArray = root.findViewById(R.id.listViewHome);
-        array = getResources().getStringArray(R.array.medik_eight);
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, array);
-        listArray.setAdapter(adapter);
+//-------listView realization
 
-        listArray.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Intent intent = new Intent(getActivity(), ContentActivity.class);
-                intent.putExtra(CATEGORY_KEY, category);
-                intent.putExtra(POSITION_KEY, position);
-                startActivity(intent);
-            }
-        });
+//        listArray = root.findViewById(R.id.listViewHome);
+//        array = getResources().getStringArray(R.array.medik_eight);
+//        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, array);
+//        listArray.setAdapter(adapter);
+
+
+        initialCosmeticItems();
+
+        RecyclerView recyclerView = root.findViewById(R.id.listViewHome);
+        DataAdapter adapter = new DataAdapter(getContext(), cosmeticItems);
+        recyclerView.setAdapter(adapter);
+
+
+
+//        listArray.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                Intent intent = new Intent(getActivity(), ContentActivity.class);
+//                intent.putExtra(CATEGORY_KEY, category);
+//                intent.putExtra(POSITION_KEY, position);
+//                startActivity(intent);
+//            }
+//        });
 
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -60,5 +72,11 @@ public class HomeFragment extends Fragment {
 //            }
 //        });
         return root;
+    }
+
+    private void initialCosmeticItems(){
+        //cosmeticItems.add(new CosmeticItem("Medik8 Illuminating Eye Balm", "49$", R.drawable.medik_0));
+        cosmeticItems.add(new CosmeticItem("Medik8 Mutiny", "29$", R.drawable.medik_1));
+        cosmeticItems.add(new CosmeticItem("Medik8 Crystal Retinal 3", "59$", R.drawable.medik_2));
     }
 }
